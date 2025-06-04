f4255f13fce19486c620df48031c436b1a623a28
signeamalie <eaa23saj@students.eaaa.dk>
2025-06-04 19:03:10 +0200

    opsaetning af #2 html side med videoer & layout

--- js/js.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-// burgermenu toggle
-const burger = document.getElementById('burger-menu');
-const mainMenu = document.getElementById('main-menu');
-
-burger.addEventListener('click', () => {
-  mainMenu.classList.toggle('active');
-});
